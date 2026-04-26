14966bc3fc7e3bdea6406a6af0fd7e498a79feb3
KrTvGG <krtviv@mail.ru>
2026-04-26 09:20:30 +0300

    16.1. Arrange Act Assert

---
+++ geo/geo_test.go
@@ -0,0 +1,11 @@
+package geo_test
+
+import "testing"
+
+func TestGetMyLocation(t *testing.T) {
+	// Arange - подготовка, expected  результат
+
+	// Act - выполняем функцию
+
+	// Assert - проверка результат с expected
+}
