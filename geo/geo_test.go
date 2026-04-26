dd24473e33a087866e39778a34b2333ab9df39a4
KrTvGG <krtviv@mail.ru>
2026-04-26 09:33:27 +0300

    16.2. Первый тест

--- geo/geo_test.go
+++ geo/geo_test.go
@@ -1,11 +1,25 @@
 package geo_test
 
-import "testing"
+import (
+	"demo/weather/geo"
+	"testing"
+)
 
 func TestGetMyLocation(t *testing.T) {
 	// Arange - подготовка, expected  результат
+	city := "London"
+	expected := geo.GeoData{
+		City: "London",
+	}
 
 	// Act - выполняем функцию
+	got, err := geo.GetMyLocation(city)
 
 	// Assert - проверка результат с expected
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if got.City != expected.City {
+		t.Errorf("Ожидаломь %v, полученом %v", expected, got)
+	}
 }
